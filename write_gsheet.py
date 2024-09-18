1c50d4de543af44a3488cf827fce2e68f4d329ce
kienvtm <kienvtm@gmail.com>
2024-09-18 18:06:06 +0700

    update sheet

--- write_gsheet.py
+++ write_gsheet.py
@@ -25,7 +25,6 @@ import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
 from gspread_dataframe import set_with_dataframe
-import pandas as pd
 
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -36,42 +35,29 @@ credentials = Credentials.from_service_account_info(
     skey,
     scopes=scopes,
 )
-
-
-# Find the first non-blank row by checking the length of the sheet
-def get_first_empty_row(worksheet):
-    str_list = list(filter(None, worksheet.col_values(1)))  # Get values in column A, ignoring empty rows
-    return len(str_list) + 1  # Next empty row (1-indexed)
+client = gspread.authorize(credentials)
 
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-# @st.cache_resource 
-# def connect_gsheet(key):
-#     return sh
-
 @st.cache_data(ttl=600)
-def load_data(sh, sheet_name="Sheet1"):
+def load_data(key, sheet_name="Sheet1"):
+    sh = client.open_by_key(key)
     df = pd.DataFrame(sh.worksheet(sheet_name).get_all_records())
     return df
 
+def update_data(worksheet, df):
+    # Find the first non-blank row by checking the length of the sheet
+    str_list = list(filter(None, worksheet.col_values(1)))
+    first_empty_row = len(str_list) + 1
+    set_with_dataframe(worksheet, df, row=first_empty_row, col=1, include_index=False, include_column_header=False)
 
 key = '1EDxslX9zP0UV2ntj0RE9fb-o6uZaSodkW_e9b0nV5uQ'
-sheet_name='Sheet1'
-client = gspread.authorize(credentials)
-sh = client.open_by_key(key)
-# sh = connect_gsheet(key)
-df = load_data(sh, sheet_name='Sheet1')
+df = load_data(key, sheet_name='Sheet1')
 st.dataframe(df)
 
-
-# Get the first empty row
-first_empty_row = get_first_empty_row(sheet_name)
-
 sheet_name='Example 1'
-worksheet = sh.worksheet(sheet_name)
 if st.button("Update worksheet"):
-# Append the DataFrame at the first empty row
-    set_with_dataframe(worksheet, df, row=first_empty_row, col=1, include_index=False, include_column_header=False)
+    update_data(sheet_name, df)
     st.cache_data.clear()
     st.rerun()
