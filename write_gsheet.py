3f00a1f0ebb18fbccfc714c77c94173b672337db
kienvtm <kienvtm@gmail.com>
2024-09-18 17:29:04 +0700

    use gspread

--- write_gsheet.py
+++ write_gsheet.py
@@ -1,21 +1,50 @@
+# import streamlit as st
+# from streamlit_gsheets import GSheetsConnection
+
+# # Create a connection object.
+# conn = st.connection("gsheets", type=GSheetsConnection)
+
+# df = conn.read(
+#     worksheet="Sheet1",
+#     ttl="10m",
+# )
+
+# # Print results.
+# st.dataframe(df)
+
+# if st.button("Update worksheet"):
+#     df = conn.update(
+#         worksheet="Example 1",
+#         data=df,
+#     )
+#     st.cache_data.clear()
+#     st.rerun()
+
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+]
 
-df = conn.read(
-    worksheet="Sheet1",
-    ttl="10m",
+skey = st.secrets["service_account"]
+credentials = Credentials.from_service_account_info(
+    skey,
+    scopes=scopes,
 )
+client = gspread.authorize(credentials)
 
-# Print results.
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(key, sheet_name="Sheet1"):
+    sh = client.open_by_key(key)
+    df = pd.DataFrame(sh.worksheet(sheet_name).get_all_records())
+    return df
+
+key = '1EDxslX9zP0UV2ntj0RE9fb-o6uZaSodkW_e9b0nV5uQ'
+df = load_data(key, sheet_name='Sheet1')
 st.dataframe(df)
-
-if st.button("Update worksheet"):
-    df = conn.update(
-        worksheet="Example 1",
-        data=df,
-    )
-    st.cache_data.clear()
-    st.rerun()
